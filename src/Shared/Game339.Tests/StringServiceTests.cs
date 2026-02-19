72505d54fda2704cac1e62a7e55c9a7ce683fcfb
Christopher Uy <123910681+chuy110@users.noreply.github.com>
2026-02-19 01:41:37 -0800

    Update StringServiceTests.cs

--- src/Shared/Game339.Tests/StringServiceTests.cs
+++ src/Shared/Game339.Tests/StringServiceTests.cs
@@ -33,3 +33,38 @@ public class StringServiceTests
         Assert.Throws<System.ArgumentNullException>(() => _svc.Reverse(null));
     }
 }
+
+public class StringServiceReverseWordsTests
+{
+    private StringService _svc;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _svc = new StringService(EmptyGameLog.Instance);
+    }
+
+    [TestCase("Good Sandy", "Sandy Good")]
+    [TestCase("one two three", "three two one")]
+    [TestCase("hello", "hello")]
+    [TestCase("", "")]
+    public void ReverseWords_ReturnsExpectedString(string input, string expected)
+    {
+        var result = _svc.ReverseWords(input);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void ReverseWords_DoesNotReverseCharactersWithinWords()
+    {
+        var result = _svc.ReverseWords("foo bar");
+        Assert.That(result, Is.EqualTo("bar foo"));
+        Assert.That(result, Is.Not.EqualTo("oof rab"));
+    }
+
+    [Test]
+    public void ReverseWords_NullInput_ThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => _svc.ReverseWords(null));
+    }
+}
